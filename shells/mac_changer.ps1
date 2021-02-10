0f15d1cef4fdc6f614e4e9e291eeb4e43a479b59
Robert Byrnes <robbyrnes@hotmail.co.uk>
2021-02-10 15:29:04 +0000

    finalising of shells

--- shells/mac_changer.ps1
+++ shells/mac_changer.ps1
@@ -1,117 +1,73 @@
-# Self-elevate the script if required
+# The following script randomizes your MAC address.
+
+# NOTE:  This script requires being as an administrator and following up with a system restart.
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if (! $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+    "This script must be run as an administrator.  It will now run with appropriate credentials."
+}
+
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
     if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
      $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
      Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-     Exit
     }
-   }
-function random_mac {   
-    $mac = "02"
-    while ($mac.length -lt 12) 
-	{ 
-		$mac += "{0:X}" -f $(get-random -min 0 -max 16) 
-	} 
-		
-    $Delimiter = "-"
-		for ($i = 0 ; $i -le 10 ; $i += 2) 
-		{ $newmac += $mac.substring($i,2) + $Delimiter }
-		$setmac = $newmac.substring(0,$($newmac.length - $Delimiter.length)) 
- $setmac
 }
 
+#First, enter the hardware MAC address for the interface whose MAC you want to randomize.  This can be obtained by using ipconfig /all in the
+# "Physical Address" field.
 
-function disconnect-wifi { 
-    $CurrentSSID = (netsh wlan show interface | Select-String 'Profile' | Foreach-Object {$_.ToString()})
-    $CurrentSSID = $CurrentSSID.trim("Connection mode        : Profile     Profile                : ")
-    Write-Host "Current WLAN SSID: $CurrentSSID"  -ForegroundColor Yellow
-
-    $WIFI = Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object { $_.IpEnabled -eq $true -and $_.DhcpEnabled -eq $true}
-    Write-Host "Releasing IP addresses:" ($WIFI.IPAddress | Select-Object -first 1)  -ForegroundColor Yellow 
-    $WIFI.ReleaseDHCPLease() | out-Null 
-
-    # Make sure the release has happened, else give it 2 sec extra. 
-    $WIFI = Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object { $_.IpEnabled -eq $true -and $_.DhcpEnabled -eq $true} 
-    if ($Null -ne $WIFI.DefaultIPGateway) { 
-    Write-Output "Release of IP Address had not completed, waiting 1 Seconds" 
-    Start-Sleep -Seconds 2
-    }
-
-    Write-Host "Disconnecting from WiFi" -ForegroundColor Yellow 
-    & netsh wlan disconnect | Out-Null
-} 
-
-
-function new-wifimac ($wifiadapter, $ssid, $newmac){ 
-
-#    Write-Output "Wifi AdapterName: $wifiadapter" 
-#    Write-Output "SSID: $ssid" 
-#    Write-Output "New MAC Address to set: $newmac" 
-
-    $oldmac = (Get-NetAdapter -Name $wifiadapter).MACAddress 
-    Write-Output "OLD MAC Address: $oldmac" 
-
-    if ($oldmac -like $newmac) {
-        Write-Host "Old MAC and New MAC are identical, generating a new MAC Address" -ForegroundColor Red
-        $newmac = random_mac
-        Write-Output "New MAC Address to set: $newmac" 
-        }
-
-    Set-NetAdapter -Name $wifiadapter -MacAddress $newmac -Confirm:$false 
-    Get-NetAdapter -Name $wifiadapter | Disable-NetAdapter -Confirm:$false -Verb runAs
-    Get-NetAdapter -Name $wifiadapter | Enable-NetAdapter -Confirm:$false -Verb runAs
-    $currentmac = (Get-NetAdapter -Name $wifiadapter).MACAddress 
-    Write-Output "NEW MAC Address: $currentmac" 
-
-    Write-Host "Connecting to SSID: $ssid" -ForegroundColor Yellow 
-    & netsh wlan connect name=$ssid ssid=$ssid
-
-    $NoIP = 0
-    Do { 
-        $WIFI = Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object { $_.IpEnabled -eq $true -and $_.DhcpEnabled -eq $true}
-        
-        if ($null -ne $WIFI.DefaultIPGateway) { 
-            $NoIP = 5
-        }
-        else {
-            Start-Sleep -Seconds 2 
-            Write-Host "Waiting for IP Address" 
-            $NoIP += 1
-        }
-    } While ($NoIP -lt 5) 
-    Write-Host "New IP addresses" ($WIFI.IPAddress | Select-Object -first 1)  -ForegroundColor Yellow 
+# A Media Access Control address is 12 hex digits.  Generally, the first 6 hex digits are the OUI (organizationally unique identifier).
+# The OUI should not be randomly generated; and should match some OUI derived from a real device.  This script will use the OUI from
+# the hardware MAC you provide.
+$HardwareMAC = "34-17-EB-4B-13-D9"
+if ($HardwareMAC -eq "")
+{
+    'Please enter a MAC address into the $HardwareMAC variable.'
+    exit
 }
 
+$HardwareMACNoDashes = $HardwareMAC.Replace("-","")
+$OUI = $HardwareMACNoDashes.Substring(0,6)
 
-function test-wifi ($probe){ 
-    if (Test-NetConnection -ComputerName $probe -CommonTCPPort HTTP -InformationLevel Quiet) { 
-        $result = "Working" 
-    } 
-    else { 
-        $result = "NotWorking" 
-    }
-$result 
+# The Windows Registry key that we want to set is in HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002BE10318}
+# At that location, there are several keys in the format 0000, 0001, 0002, etc.  Prior to running the script, browse to the key above and
+# determine which of the keys inside contains your NIC.  The DriverDesc value contains a description of the NIC.  Set NICKey appropriately.
+# SUCCESSFUL EXECUTION OF THIS SCRIPT MAY REQUIRE CHANGING USER PERMISSIONS IN "0001" i.e. CORRECT LOCATION FOR NICkey
+$NICKey="0001"
+
+# Obtain 6 randomly-generated seeds to assist with PRNG generation of each hex digit
+$SeedsString = (Invoke-WebRequest -URI "https://www.random.org/integers/?num=6&min=1&max=1000000000&col=1&base=10&format=plain&rnd=new").Content.Split("`r`n")
+
+# Generate 6-hex-digit string
+function Get-RandomHexDigit {
+    Param([Parameter(Mandatory=$True)] $Seed)
+    '{0:x}' -f (Get-Random -SetSeed $Seed -Min 1 -Max 15)
 }
 
+$GenDigits=@()
 
-# Specify $SSID manually 
-# $ssid = 'SSID-to-Connect-to' 
-# 
-# Or use the currently used SSID to reconnect to. 
-$ssid = (& netsh wlan show interfaces | Select-String ' SSID ' | Foreach-Object {$_.ToString()}).replace("    SSID                   : ","$null")
+For ($i = 0; $i -lt 6; $i++)
+{    
+    $GenDigits = "$GenDigits$(Get-RandomHexDigit -Seed $SeedsString[$i])"
+}
 
-# Specify WLAN Adapter Name Manually 
-# $wifiadapter = 'RTWlanE'
-# 
-# Or Try to identify the Wi-Fi Adapter 
-$wifiadapter = (Get-NetAdapter | where Status -EQ "Up" | where MediaType -EQ "802.11" |  Where { $_.IpEnabled -eq $true -and $_.DhcpEnabled -eq $true} | select *).MacAddress.replace(":","-").Name
-#  where MacAddress -EQ (Get-WmiObject -Class Win32_NetworkAdapterConfiguration |
-# Specify a MAC Address manually
-# $newmac = "02-F4-D7-B2-FE-D8"
-#
-# Or generate a new Random MAC Address
-$newmac = random_mac
+$GenDigitsString = $GenDigits.ToString()
+$GenDigitsString = $GenDigitsString.ToUpper()
 
-disconnect-wifi
-new-wifimac -wifiadapter $wifiadapter -ssid $ssid -newmac $newmac 
-test-wifi -probe www.msftncsi.com
+$NewMACAddress = "$OUI$GenDigitsString"
+
+$OldMACAddress = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002BE10318}\$NICKey").NetworkAddress
+
+
+#Disable/reenable the NIC to enable the new MAC address to take effect
+$Adapter = Get-NetAdapter | Where-Object{$_.MacAddress -eq $HardwareMAC}
+$Adapter | Disable-NetAdapter -Confirm:$false
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002BE10318}\$NICKey" -Name "NetworkAddress" -Value $NewMACAddress
+$Adapter | Enable-NetAdapter -Confirm:$false
+$Adapter | Restart-NetAdapter -Confirm:$false
+
+"Done.
+Adapter specified:"
+$Adapter | Select-Object Name,InterfaceDescription,InterfaceIndex,MacAddress | Format-List
+"Old MAC Address:$OldMACAddress -> New MAC Address:$NewMACAddress"
